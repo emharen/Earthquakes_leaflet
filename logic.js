1e8eaf4255291f62784412550f4fd2b4817e6940
Erika Haren <emharen@hotmail.com>
2019-06-05 22:21:28 -0400

    update

--- logic.js
+++ logic.js
@@ -125,6 +125,40 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 
-  
+  // Create legend
+  var legend = L.control({position: 'bottomright'});
 
+  legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+              grades = [0, 1, 2, 3, 4, 5],
+              labels = [];
+
+  // Loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+
+  legend.addTo(myMap);
+
+  // Color
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+        return '#CC0000'
+    } else if (magnitude > 4) {
+        return '#FF6900'
+    } else if (magnitude > 3) {
+        return '#FFCC00'
+    } else if (magnitude > 2) {
+        return '#CCFF33'
+    } else if (magnitude > 1) {
+        return 'green'
+    } else {
+        return '#66FFCC'
+    }
+  }
 }
